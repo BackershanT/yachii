3588ce1772026495c3fe0fb45e92deef327cec66
Backershan T <98761199+BackershanT@users.noreply.github.com>
2025-08-15 02:03:00 +0530

    asd

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '@/pages/Home/Home';
-import Career from '@/pages/Career/career';
+import Career from '@/pages/Career/Career';
 import Layout from '@/layouts/Layout';
 
 const AppRoutes = () => {
